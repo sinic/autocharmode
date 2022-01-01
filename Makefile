8040583725ee2e19fb78beede17d376435fd4287
Simon Nicolussi <sinic@sinic.name>
2022-01-01 17:26:05 +0100

    Import into public repository

---
+++ Makefile
@@ -0,0 +1,17 @@
+CFLAGS += $(shell pkg-config --cflags gtk+-3.0)
+LDFLAGS += $(shell pkg-config --libs-only-L gtk+-3.0)
+LDLIBS += $(shell pkg-config --libs-only-l gtk+-3.0)
+
+dir = immodules
+
+.PHONY: all clean
+all: immodules.cache
+clean:
+	$(RM) -r $(dir) immodules.cache
+
+$(dir):
+	mkdir $(dir)
+$(dir)/%.so: %.c $(dir)
+	$(CC) -fPIC -shared -o $@ $(CFLAGS) $(LDFLAGS) $< $(LDLIBS)
+immodules.cache: $(dir)/charmode.so
+	gtk-query-immodules-3.0 $< > $@
---
+++ charmode.c
@@ -0,0 +1,82 @@
+/* Copyright (C) 2021 Simon Nicolussi
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library. If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "gdk/gdkx.h"
+#include "glib.h"
+#include "glib/gprintf.h"
+#include "gtk/gtk.h"
+#include "gtk/gtkimmodule.h"
+
+GType type = 0;
+
+typedef struct {
+  GtkIMContextSimple parent;
+  Window window;
+} CharModeContext;
+
+static void toggle(const gchar *action, GtkIMContext *context) {
+  gchar *s = g_strdup_printf("emacsclient -eu (exwm-input-%s-keyboard#x%lx)",
+                             action, ((CharModeContext *)context)->window);
+  g_spawn_command_line_sync(s, NULL, NULL, NULL, NULL);
+  g_free(s);
+}
+
+static void focus_in(GtkIMContext *context) {
+  toggle("release", context);
+}
+
+static void focus_out(GtkIMContext *context) {
+  toggle("grab", context);
+}
+
+static void set_client_window(GtkIMContext *context, GdkWindow *window) {
+  CharModeContext *sub = (CharModeContext *)context;
+
+  sub->window = window == NULL ? 0 : gdk_x11_window_get_xid(window);
+}
+
+static void init(gpointer g_class) {
+  GTK_IM_CONTEXT_CLASS(g_class)->focus_in = focus_in;
+  GTK_IM_CONTEXT_CLASS(g_class)->focus_out = focus_out;
+  GTK_IM_CONTEXT_CLASS(g_class)->set_client_window = set_client_window;
+}
+
+G_MODULE_EXPORT
+void im_module_init(GTypeModule *module) {
+  static const GTypeInfo info = { .class_size = sizeof(GtkIMContextSimpleClass),
+    .base_init = init, .instance_size = sizeof(CharModeContext) };
+
+  type = g_type_module_register_type(module, GTK_TYPE_IM_CONTEXT_SIMPLE,
+                                     "CharModeContext", &info, 0);
+}
+
+G_MODULE_EXPORT
+void im_module_exit(void) {
+}
+
+G_MODULE_EXPORT
+void im_module_list(const GtkIMContextInfo ***contexts, int *n_contexts) {
+  static const GtkIMContextInfo info = {"charmode", "Toggle EXWM char-mode",
+                                        "", "", ""},
+    *info_list[] = { &info };
+
+  *contexts = info_list;
+  *n_contexts = G_N_ELEMENTS(info_list);
+}
+
+G_MODULE_EXPORT
+GtkIMContext *im_module_create(const gchar *context_id) {
+  return g_strcmp0(context_id, "charmode") ? NULL : g_object_new(type, NULL);
+}
